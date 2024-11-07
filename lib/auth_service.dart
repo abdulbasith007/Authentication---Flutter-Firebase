4b2edadb66b85cfff86bb218dfeaef5d61ef095c
Basith Abdul <abdulbasithCOC@gmail.com>
2024-11-07 18:09:03 -0500

    Added auth service to take care of authentication

---
+++ lib/auth_service.dart
@@ -0,0 +1,43 @@
+
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // Current user
+  User? get currentUser => _firebaseAuth.currentUser;
+
+  // Sign-in method
+  Future<User?> signIn(String email, String password) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print('Error during sign-in: $e');
+      return null;
+    }
+  }
+
+  // Register method
+  Future<User?> register(String email, String password) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print('Error during registration: $e');
+      return null;
+    }
+  }
+
+  // Sign-out method
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
